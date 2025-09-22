fb091811d0010f193e645bc0ebf87e6ebe14551f
jbihamba <josephbihamba25@gmail.com>
2025-09-22 23:37:13 +0100

    feat: export variables for exercice 7a tests to pass

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -5,8 +5,20 @@ let agePere = ageEnfant * 2;
 let ageMere = agePere - 5;
 let ageGrandPere = (2 * ageMere) + (0.5 * ageEnfant);
 let ageOncle = agePere + 10;
+
 console.log("Âge de l'enfant:", ageEnfant);
 console.log("Âge du père:", agePere);
 console.log("Âge de la mère:", ageMere);
 console.log("Âge du grand-père:", ageGrandPere);
 console.log("Âge de l'oncle:", ageOncle);
+
+// --- Export des variables pour les tests ---
+if (typeof module !== "undefined") {
+  module.exports = {
+    ageEnfant,
+    agePere,
+    ageMere,
+    ageGrandPere,
+    ageOncle
+  };
+}
