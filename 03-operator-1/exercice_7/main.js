2db33abc3a3dd7d5077aee09b15074f955ab7fb8
jbihamba <josephbihamba25@gmail.com>
2025-09-22 21:25:15 +0100

    feat: assign and calculate ages for child, parents, grandfather, and uncle

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,12 @@
 // Exercice 7
 // Écrivez votre code ici
+let ageEnfant = 10;
+let agePere = ageEnfant * 2;
+let ageMere = agePere - 5;
+let ageGrandPere = (2 * ageMere) + (0.5 * ageEnfant);
+let ageOncle = agePere + 10;
+console.log("Âge de l'enfant:", ageEnfant);
+console.log("Âge du père:", agePere);
+console.log("Âge de la mère:", ageMere);
+console.log("Âge du grand-père:", ageGrandPere);
+console.log("Âge de l'oncle:", ageOncle);
