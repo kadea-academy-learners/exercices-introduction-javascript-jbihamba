8bb1ca11cca51e276f0bf4cddb8b323ea6b7f216
jbihamba <josephbihamba25@gmail.com>
2025-09-22 21:46:11 +0100

    feat: calculate monthly expenses and remaining salary

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,2 +1,12 @@
 // Exercice 8
 // Écrivez votre code ici
+let salaireMensuel = 500;
+let loyer = (30 / 100) * 500;
+let nouriture = (20 /100) * 500;
+let transport =(10/100) * 500;
+let autreDepense =50;
+autreDepense=autreDepense+25;
+let totalDepense = salaireMensuel + loyer + nouriture + transport + autreDepense;
+console.log("Total de depense est :" + totalDepense)
+let reste = salaireMensuel -totalDepense;
+console.log("Reste est :" + reste)
