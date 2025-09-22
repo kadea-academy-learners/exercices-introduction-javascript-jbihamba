7d16691f61d4997874005a55c32039a99503486f
jbihamba <josephbihamba25@gmail.com>
2025-09-22 20:55:59 +0100

    feat: store Celsius in a variable using let and convert to Fahrenheit

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,10 @@
 // Exercice 5
 // Écrivez votre code ici
+let celsius = 25;
+
+// formule de Fahrenheit
+
+let fahrenheit = (celsius * 9/5) + 32;
+
+console.log(celsius + "°C = " + fahrenheit + "°F");
+
